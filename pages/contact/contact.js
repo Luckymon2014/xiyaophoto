036c89d1f857ef4ad08bbf885245dd1ccb3cb90d
shanxiao <shan_xiao0823@hotmail.com>
2018-05-14 15:11:38 +0800

    v1.3

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -6,14 +6,14 @@ Page({
         "photo": "/resources/icons/contact-missing.png",
         "qq": "123456789",
         "wechat": "helloworld",
-        "weibo": "摄影师李逍遥"
+        "weibo": "摄影师李肖遥"
       },
       "xiaoye": {
         "name": "削削",
         "photo": "/resources/icons/contact-missing.png",
         "qq": "987654321",
         "wechat": "helloworld~",
-        "weibo": "雨后风荷"
+        "weibo": "削削_雨后风荷"
       }
     }
   }
