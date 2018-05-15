ab8cbb3ffca97c8c251eb423e6f84107cbecac34
shanxiao <shan_xiao0823@hotmail.com>
2018-05-15 13:48:59 +0800

    联系信息更新，联系信息可空

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -2,18 +2,30 @@ Page({
   data: {
     "contact": {
       "xiaoyao": {
-        "name": "李肖遥",
-        "photo": "/resources/icons/contact-missing.png",
-        "qq": "123456789",
-        "wechat": "helloworld",
+        "name": "摄影师：李肖遥",
+        "photo": "/resources/contact/xiaoyao.png",
+        "qq": "512244021",
+        "wechat": "xiaowhitehaven",
         "weibo": "摄影师李肖遥"
       },
       "xiaoye": {
-        "name": "削削",
-        "photo": "/resources/icons/contact-missing.png",
-        "qq": "987654321",
-        "wechat": "helloworld~",
+        "name": "摄影师：削削",
+        "photo": "/resources/contact/xiaoxiao.png",
+        "qq": "695721397",
+        "wechat": "yuhou207fenghe~",
         "weibo": "削削_雨后风荷"
+      },
+      "naicha": {
+        "name": "妆造师：奶茶",
+        "photo": "/resources/contact/naicha.png",
+        "qq": "1091490695",
+        "wechat": "naichacha227",
+        "weibo": "卖水晶的奶茶"
+      },
+      "xiaolu": {
+        "name": "模特：小鹿",
+        "photo": "/resources/contact/xiaolu.png",
+        "weibo": "温柔少女鹿 ​​​​"
       }
     }
   }
