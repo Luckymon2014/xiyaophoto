5142cb1e5f9fe16da674ac30f1b34f1c0206cde7
shanxiao <shan_xiao0823@hotmail.com>
2018-05-18 13:42:24 +0800

    实现滑动日历功能

--- pages/calendar/calendar.js
+++ pages/calendar/calendar.js
@@ -1,4 +1,4 @@
-var fileData = require('../../resources/data.js'); 
+var fileData = require('../../resources/data.js');
 
 const today = new Date();
 Page({
@@ -64,6 +64,86 @@ Page({
 	prev: function (e) {
 		this.selectDays(this.data.appointments, e.detail.currentYear, e.detail.currentMonth);
 	},
+	// 选择下个月事件
+	toNextMonth: function () {
+		const curYear = this.data.year;
+		const curMonth = this.data.month;
+		let newMonth = curMonth + 1;
+		let newYear = curYear;
+		if (newMonth > 12) {
+			newYear = curYear + 1;
+			newMonth = 1;
+		}
+		this.setData({
+			'year': newYear,
+			'month': newMonth,
+		});
+	},
+	// 选择上个月事件
+	toPrevMonth: function () {
+		const curYear = this.data.year;
+		const curMonth = this.data.month;
+		let newMonth = curMonth - 1;
+		let newYear = curYear;
+		if (newMonth < 1) {
+			newYear = curYear - 1;
+			newMonth = 12;
+		} this.setData({
+			'year': newYear,
+			'month': newMonth,
+		});
+	},
+	// 滑动事件
+	calendarTouchstart(e) {
+		const t = e.touches[0];
+		const startX = t.clientX;
+		const startY = t.clientY;
+		this.slideLock = true; // 滑动事件加锁
+		this.setData({
+			'gesture.startX': startX,
+			'gesture.startY': startY
+		});
+	},
+	calendarTouchmove(e) {
+		if (this.isLeftSlide.call(this, e)) {
+			this.toNextMonth();
+			this.selectDays(this.data.appointments, this.data.year, this.data.month);
+		}
+		if (this.isRightSlide.call(this, e)) {
+			this.toPrevMonth();
+			this.selectDays(this.data.appointments, this.data.year, this.data.month);
+		}
+	},
+	// 左滑
+	isLeftSlide: function (e) {
+		const { startX, startY } = this.data.gesture;
+		if (this.slideLock) {
+			const t = e.touches[0];
+			const deltaX = t.clientX - startX;
+			const deltaY = t.clientY - startY;
+			if (deltaX < -60 && deltaY < 20 && deltaY > -20) {
+				this.slideLock = false;
+				return true;
+			} else {
+				return false;
+			}
+		}
+	},
+	// 右滑
+	isRightSlide: function (e) {
+		const { startX, startY } = this.data.gesture;
+		if (this.slideLock) {
+			const t = e.touches[0];
+			const deltaX = t.clientX - startX;
+			const deltaY = t.clientY - startY;
+			if (deltaX > 60 && deltaY < 20 && deltaY > -20) {
+				this.slideLock = false;
+				return true;
+			} else {
+				return false;
+			}
+		}
+	},
 	// 点击已预约日期对象事件
 	// clickAppoint: function (e) {
 	// 	var appoint = e.currentTarget.dataset;
@@ -72,5 +152,5 @@ Page({
 	// 		"month": appoint.month,
 	// 	});
 	// 	this.selectDays(this.data.appointments, this.data.year, this.data.month);
-	// }
+	// },
 })
