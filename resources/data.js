cb84e8a4b426e4e6a164df75456db5e9defec72c
Luckymon2014 <shan_xiao0823@hotmail.com>
2018-11-14 18:15:04 +0800

    服装 日常 照片更新

--- resources/data.js
+++ resources/data.js
@@ -116,10 +116,10 @@ function mtData() {
     else
       dressListHanfu2[(i - 1) / 2] = new Dress("汉服", i + 1);
   }
-  // 日常 9
+  // 日常 26
   var dressListRichang1 = new Array();
   var dressListRichang2 = new Array();
-  for (var i = 0; i < 9; i++) {
+  for (var i = 0; i < 26; i++) {
     if (i % 2 == 0)
       dressListRichang1[i / 2] = new Dress("日常", i + 1);
     else
